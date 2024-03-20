0c7fdbbb3863db4741de7ffeacfdf034d700dd84
vrushalimohite <vrushalimohite223@gmail.com>
2024-03-20 10:48:37 +0530

    Add project files.

---
+++ Clinic_management_system_mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Clinic_management_system_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
