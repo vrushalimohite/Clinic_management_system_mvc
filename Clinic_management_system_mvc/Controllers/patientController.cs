8472ee1718c21f3d5335bf4ec6432e80c71eae13
vrushalimohite <vrushalimohite223@gmail.com>
2024-03-22 17:11:36 +0530

    add

---
+++ Clinic_management_system_mvc/Controllers/patientController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Clinic_management_system_mvc.Controllers
+{
+	public class patientController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
