2e51c186939100545f54464e4dca902016cc3a17
vrushalimohite <vrushalimohite223@gmail.com>
2024-03-26 12:03:43 +0530

    add

--- Clinic_management_system_mvc/Controllers/DoctorController.cs
+++ Clinic_management_system_mvc/Controllers/DoctorController.cs
@@ -1,12 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Clinic_management_system_mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Clinic_management_system_mvc.Controllers
 {
-	public class DoctorController : Controller
-	{
-		public IActionResult Index()
-		{
-			return View();
-		}
-	}
-}
+    public class DoctorController : Controller
+    {
+           [HttpGet]
+            public ActionResult Registration()
+            {
+                return View(new Doctor());
+            }
+
+            [HttpPost]
+            public ActionResult Register(Doctor doctor)
+            {
+                if (ModelState.IsValid)
+                {
+                    // Save doctor to database or perform other actions
+                    // Redirect to success page or another action
+                    return RedirectToAction("RegistrationSuccess");
+                }
+                // If validation fails, return to the registration form with errors
+                return View("Registration", doctor);
+            }
+
+            public ActionResult RegistrationSuccess()
+            {
+                return View();
+            }
+       
+            public ActionResult Index()
+            {
+                var model = new MyDropDownModel
+                {
+                    Items = new List<SelectListItem>
+            {
+                new SelectListItem { Value = "1", Text = "Option 1" },
+                new SelectListItem { Value = "2", Text = "Option 2" },
+                new SelectListItem { Value = "3", Text = "Option 3" }
+            }
+                };
+
+                ViewBag.MyDropDownModel = model;
+
+                return View();
+            }
+        }
+
+    }
+
+
+
---
+++ Clinic_management_system_mvc/Models/Doctor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Clinic_management_system_mvc.Models
+{
+	public class Doctor
+	{
+		
+			public string Name { get; set; }
+			public string Email { get; set; }
+			public string Specialty { get; set; }
+			// Add more properties as needed
+		
+
+	}
+    public class MyDropDownModel
+    {
+        public int SelectedItemId { get; set; }
+        public List<SelectListItem> Items { get; set; }
+    }
+
+}
