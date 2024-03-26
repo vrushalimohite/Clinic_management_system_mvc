e93e05706286034b2139b0a0bd5a40faec08d9ac
mayurikale2406 <mayurikale1404@gmail.com>
2024-03-26 11:25:29 +0530

    add

--- Clinic_management_system_mvc/Controllers/patientController.cs
+++ Clinic_management_system_mvc/Controllers/patientController.cs
@@ -8,5 +8,31 @@ namespace Clinic_management_system_mvc.Controllers
 		{
 			return View();
 		}
-	}
+
+	 // GET: Patient/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: Patient/Register
+        [HttpPost]
+        public ActionResult Register(patientController patient)
+        {
+            // Save patient data to the database or perform other actions
+            // For demonstration, let's assume we save the patient to a database
+            // patientRepository.Save(patient);
+
+            // Redirect to a success page or show a success message
+            return RedirectToAction("RegistrationSuccess");
+        }
+
+        // GET: Patient/RegistrationSuccess
+        public ActionResult RegistrationSuccess()
+        {
+            return View();
+        }
+    }
+
 }
+
---
+++ Clinic_management_system_mvc/Models/Patient.cs
@@ -0,0 +1,16 @@
+﻿namespace Clinic_management_system_mvc.Models
+{
+    public class Patient
+    {
+        
+            public int Patient Id { get; set; }
+
+            public string First Name { get; set; }
+            public string LastName { get; set }
+            public int Age { get; set; }
+            public string Address { get; set; }
+           
+        
+
+    }
+}
